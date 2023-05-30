8593aa3d1c75333ddfeedbee4bd84bf59e357f33
yuuexa <91273572+yuuexa@users.noreply.github.com>
2023-05-30 21:36:22 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -30,39 +30,52 @@ lesson = {
     "B金曜日の時間割": ["地理総合", "体育", "S.E.P(物理)", "保健", "音楽"],
 }
 
-exam_range = {
-    "英コミュの小テスト範囲": {
-        "5月29日": "No222 - 340(書き)",
-        "6月1日": "No341 - 400",
-        "6月6日": "No401 - 455",
-        "6月7日": "No341 - 455(書き)",
-    },
-    "論理表現の小テスト範囲": {
-        "5月23日": "No52 - 59",
-        "6月2日": "No60 - 67",
-        "6月7日": "No68 - 77",
-        "6月8日": "No78 - 91",
-        "6月16日": "No92 - 104",
-        "6月21日": "No104 - 111",
-        # "6月22日": "No112 - 127",
-        # "6月30日": "No128 - 136",
-        # "7月24日": "No137 - 153",
-        # "6月24日": "No154 - 168",
-        # "6月24日": "No169 - 184",
-    },
-    "言語文化の小テスト範囲": {
-        "5月22日": "No51 - 60",
-        "5月31日": "No61 - 70",
-        "6月1日": "No71 - 80",
-        "6月5日": "全範囲",
-        "6月12日": "?"
-    },
-    "現代の国語の小テスト範囲": {
-        "5月30日": "p.38 - 41",
-        "6月1日": "p.42 - 45",
-        "6月6日": "全範囲(p.14 - 45)",
-    }
+admin = {
+    "課題追加": "[課題名] [教科] [期限]",
+    "課題作成": "[課題名] [教科] [期限]",
+    "課題削除": "[課題ID]",
+    "お知らせ追加": "[種類] [内容] [日付]",
+    "お知らせ作成": "[種類] [内容] [日付]",
+    "お知らせ削除": "[お知らせID]",
+    "テスト追加": "[範囲] [教科] [日付]",
+    "テスト作成": "[範囲] [教科] [日付]",
+    "テスト削除": "[テストID]",
 }
+
+# exam_range = {
+#     "英コミュの小テスト範囲": {
+#         "5月29日": "No222 - 340(書き)",
+#         "5月30日": "",
+#         "6月1日": "No341 - 400",
+#         "6月6日": "No401 - 455",
+#         "6月7日": "No341 - 455(書き)",
+#     },
+#     "論理表現の小テスト範囲": {
+#         "5月23日": "No52 - 59",
+#         "6月2日": "No60 - 67",
+#         "6月7日": "No68 - 77",
+#         "6月8日": "No78 - 91",
+#         "6月16日": "No92 - 104",
+#         "6月21日": "No104 - 111",
+#         # "6月22日": "No112 - 127",
+#         # "6月30日": "No128 - 136",
+#         # "7月24日": "No137 - 153",
+#         # "6月24日": "No154 - 168",
+#         # "6月24日": "No169 - 184",
+#     },
+#     "言語文化の小テスト範囲": {
+#         "5月22日": "No51 - 60",
+#         "5月31日": "No61 - 70",
+#         "6月1日": "No71 - 80",
+#         "6月5日": "全範囲",
+#         "6月12日": "?"
+#     },
+#     "現代の国語の小テスト範囲": {
+#         "5月30日": "p.38 - 41",
+#         "6月1日": "p.42 - 45",
+#         "6月6日": "全範囲(p.14 - 45)",
+#     }
+# }
 
 @app.route("/")
 def hello_world():
@@ -205,6 +218,65 @@ def remove_news(id):
     cursor.execute(f"DELETE FROM news WHERE id = '{id}'")#executeコマンドでSQL文を実行
     conn.commit()#コミットする
 
+# テスト範囲関連
+def add_tests(event, range, subject, date):
+    #DB設定
+    conn = sqlite3.connect('tests.db')#データベースを作成、自動コミット機能ON
+    cursor = conn.cursor() #カーソルオブジェクトを作成
+
+    sql = """CREATE TABLE IF NOT EXISTS tests(range, subject, date, author, id)"""
+    cursor.execute(sql)#executeコマンドでSQL文を実行
+    conn.commit()#データベースにコミット(Excelでいう上書き保存。自動コミット設定なので不要だが一応・・)]
+
+    sql = """INSERT INTO tests VALUES(?, ?, ?, ?, ?)"""#?は後で値を受け取るよという意味
+
+    profile = line_bot_api.get_profile(event.source.user_id)
+    data = ((range, subject, date, profile.display_name, event.message.id))#挿入するレコードを指定
+    cursor.execute(sql, data)#executeコマンドでSQL文を実行
+    conn.commit()#コミットする
+
+def show_tests(subject):
+    conn = sqlite3.connect('tests.db')#データベースを作成、自動コミット機能ON
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor() #カーソルオブジェクトを作成
+
+    sql = """CREATE TABLE IF NOT EXISTS tests(range, subject, date, author, id)"""
+    cursor.execute(sql)#executeコマンドでSQL文を実行
+    conn.commit()#データベースにコミット(Excelでいう上書き保存。自動コミット設定なので不要だが一応・・)]
+
+    if (subject == '今日'):
+        cursor.execute(f'SELECT * FROM tests WHERE (date = "{datetime.date.today().strftime("%m-%d")}") ORDER BY date ASC')
+    elif (subject == '明日'):
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+        cursor.execute(f'SELECT * FROM tests WHERE (date = "{tomorrow.strftime("%m-%d")}") ORDER BY date ASC')
+    else:
+        cursor.execute(f'SELECT * FROM tests WHERE (subject = "{subject}" AND date >= "{datetime.date.today().strftime("%m-%d")}") ORDER BY date ASC')
+
+    result = [dict(row) for row in cursor.fetchall()]
+
+    tests = []
+
+    if subject == ('今日' or '明日'):
+        for i in result:
+            tests.append(f'《{i["subject"]}》 {i["range"]}')
+    else:
+        for i in result:
+            date = f'{i["date"].split("-")[0]}月{i["date"].split("-")[1]}日'
+            tests.append(f'{date} {i["range"]}')
+    return tests
+
+def remove_tests(id):
+    #DB設定
+    conn = sqlite3.connect('tests.db')#データベースを作成、自動コミット機能ON
+    cursor = conn.cursor() #カーソルオブジェクトを作成
+
+    sql = """CREATE TABLE IF NOT EXISTS tests(range, subject, date, author, id)"""
+    cursor.execute(sql)#executeコマンドでSQL文を実行
+    conn.commit()#データベースにコミット(Excelでいう上書き保存。自動コミット設定なので不要だが一応・・)]
+
+    cursor.execute(f"DELETE FROM tests WHERE id = '{id}'")#executeコマンドでSQL文を実行
+    conn.commit()#コミットする
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message(event)
@@ -216,8 +288,22 @@ def handle_message(event):
 
     elif event.message.text == 'テスト範囲':
         subject_list = ["英コミュ", "論理表現", "言語文化", "現代の国語", "今日", "明日"]
-        items = [QuickReplyButton(action = MessageAction(label = f"{subject}", text = f"{subject}の小テスト範囲")) for subject in subject_list]
+        items = [QuickReplyButton(action = MessageAction(label = f"{test}", text = f"テスト範囲 {test}")) for test in subject_list]
         reply_message = TextSendMessage(text = "何の範囲かな？",quick_reply=QuickReply(items=items))
+
+    elif (event.message.text.split(' ')[0] == ('テスト範囲')) and (event.message.text.split(' ')[1] is not None):
+        if event.message.text.split(' ')[1] == '今日':
+            test = '\n\n'.join(show_tests('今日')) or 'なし'
+            day = f"{datetime.datetime.now().month}月{datetime.datetime.now().day}日"
+            reply_message = TextMessage(text = f'{day} のテスト範囲\n{test}')
+        elif event.message.text.split(' ')[1] == '明日':
+            test = '\n\n'.join(show_tests('明日')) or 'なし'
+            day = f"{(datetime.datetime.now() + datetime.timedelta(days = 1)).month}月{(datetime.datetime.now() + datetime.timedelta(days = 1)).day}日"
+            reply_message = TextMessage(text = f'{day} のテスト範囲\n{test}')
+        else:
+            day = f"{datetime.datetime.now().month}月{datetime.datetime.now().day}日"
+            test = '\n\n'.join(show_tests(event.message.text.split(' ')[1])) or 'なし'
+            reply_message = TextMessage(text = f'{day}以降の {event.message.text.split(" ")[1]}\n{test}')
 
     elif event.message.text == '課題':
         day = f"{datetime.datetime.now().month}月{datetime.datetime.now().day}日"
@@ -238,28 +324,8 @@ def handle_message(event):
     elif event.message.text in lesson:
         reply_message = TextSendMessage(text = f"{event.message.text}\n① {lesson[event.message.text][0]}\n② {lesson[event.message.text][1]}\n③ {lesson[event.message.text][2]}\n④ {lesson[event.message.text][3]}\n⑤ {lesson[event.message.text][4]}")
 
-    elif event.message.text in exam_range:
-        ranges = []
-        for key, value in exam_range[event.message.text].items():
-            ranges.append(f"{key} {value}")
-        range = '\n'.join(ranges)
-        reply_message = TextSendMessage(text = f'{event.message.text}\n{range}')
-
-    elif event.message.text == '明日の小テスト範囲':
-        day = f"{(datetime.datetime.now() + datetime.timedelta(days = 1)).month}月{(datetime.datetime.now() + datetime.timedelta(days = 1)).day}日"
-        communication = exam_range["英コミュの小テスト範囲"].get(day) or "なし"
-        expression = exam_range["論理表現の小テスト範囲"].get(day) or "なし"
-        culture = exam_range["言語文化の小テスト範囲"].get(day) or "なし"
-        modern_japanese = exam_range["現代の国語の小テスト範囲"].get(day) or "なし"
-        reply_message = TextSendMessage(text = f'{day} の小テスト範囲\n【英コミュ】 {communication}\n【論理表現】 {expression}\n【言語文化】 {culture}\n【現代の国語】 {modern_japanese}')
-
-    elif event.message.text == '今日の小テスト範囲':
-        day = f"{datetime.datetime.now().month}月{datetime.datetime.now().day}日"
-        communication = exam_range["英コミュの小テスト範囲"].get(day) or "なし"
-        expression = exam_range["論理表現の小テスト範囲"].get(day) or "なし"
-        culture = exam_range["言語文化の小テスト範囲"].get(day) or "なし"
-        modern_japanese = exam_range["現代の国語の小テスト範囲"].get(day) or "なし"
-        reply_message = TextSendMessage(text = f'{day} の小テスト範囲\n【英コミュ】 {communication}\n【論理表現】 {expression}\n【言語文化】 {culture}\n【現代の国語】 {modern_japanese}')
+    elif event.message.text in admin:
+        reply_message = TextSendMessage(text = f'「{event.message.text}」の使用方法\n{admin[event.message.text]}')
 
     elif event.message.text.split(' ')[0] == ('課題追加' or '課題作成'):
         task_name = event.message.text.split(' ')[1]
@@ -279,6 +345,18 @@ def handle_message(event):
         news_date = event.message.text.split(' ')[3]
         add_news(event, news_type, news_body, news_date)
         reply_message = TextMessage(text = f'{news_date} の 【{news_type}】 {news_body} を追加しました。\n{event.message.id}')
+
+    elif event.message.text.split(' ')[0] == ('テスト追加' or 'テスト作成'):
+        test_range = event.message.text.split(' ')[1]
+        test_subject = event.message.text.split(' ')[2]
+        test_date = event.message.text.split(' ')[3]
+        add_tests(event, test_range, test_subject, test_date)
+        reply_message = TextMessage(text = f'{test_date}に{test_subject}のテスト{test_range}を追加しました。\n{event.message.id}')
+
+    elif event.message.text.split(' ')[0] == 'テスト削除':
+        test_id = event.message.text.split(' ')[1]
+        remove_news(test_id)
+        reply_message = TextMessage(text = f'テスト {test_id} を削除しました。')
 
     elif event.message.text.split(' ')[0] == 'お知らせ削除':
         news_id = event.message.text.split(' ')[1]
